73e9557317998d7050d51581bfa9af849ab562e5
SailMaster91 <sail91@gmail.com>
2026-04-27 21:00:36 +0200

    Agregando projecto a repositorio de claude

--- utils/parsing_utils.c
+++ utils/parsing_utils.c
@@ -3,14 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chguerre <chguerre@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: chguerre <chguerre@student.lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 17:10:38 by chguerre          #+#    #+#             */
-/*   Updated: 2026/04/27 17:39:10 by chguerre         ###   ########.fr       */
+/*   Updated: 2026/04/27 20:55:52 by chguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void add_to_tail(t_node *node, t_master *master_s)
+{
+	t_node *current_node;
+	if(master_s->tail_a == NULL)
+	{
+		master_s->tail_a->prev= current_node->prev;
+		master_s->tail_a->next = current_node->next;
+		master_s->tail_a = current_node;
+	}
+	else
+
+}
+
+t_node *create_node(int num)
+{
+	t_node *new_node;
+
+	new_node = malloc(sizeof(t_node));
+	if(new_node == NULL)
+		return(NULL);
+	new_node->contain = num;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return(new_node);
+}
 
 long	ft_strtol(char *str, int *error)
 {
